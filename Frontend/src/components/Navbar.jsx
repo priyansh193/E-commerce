d7f6a28d0f3e964aac7840eb0416f01c4fc8673e
Priyansh Gupta <priyanshgupta193@gmail.com>
2025-01-19 23:29:33 +0530

    collectionPageComplete

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { ShopContext } from '../context/ShopContext'
 
 function Navbar() {
 
     const [visible, setVisible] = useState(false)
+
+    const {setShowSearch} = useContext(ShopContext)
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -32,7 +35,7 @@ function Navbar() {
         </ul>
 
         <div className='flex items-center gap-6'>
-            <img src="#" className='w-5 cursor pointer' alt="Search" />
+            <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor pointer' alt="Search" />
 
             <div className='group relative'>
                 <img src="#" className='w-5 cursor-pointer' alt="Profile" />
