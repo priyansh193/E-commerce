a5fd2ca7c688e3505c519d606ba3f967f9074d86
Priyansh Gupta <priyanshgupta193@gmail.com>
2025-01-20 23:43:03 +0530

    OrderPageAdded

--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -2,13 +2,15 @@ import React, { useContext, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 function Product() {
 
   const {productId} = useParams();
-  const {products} = useContext(ShopContext)
+  const {products,currency, AddToCart} = useContext(ShopContext)
   const [productData, setProductData] = useState(false)
   const [image, setImage] = useState('')
+  const [size,setSize] = useState('')
 
   const fetchProductData = async () => {
     products.map((item)=>{
@@ -55,9 +57,44 @@ function Product() {
             <img src={assets.star_dull_icon} alt=""  className='w-3 5'/>
             <p className='pl-2'>(122)</p>
           </div>
-          
+          <p mt-5 text-3xl font-medium>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:4/5'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Select Size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item,index) => (
+                <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-300 ${item === size ? "border-orange-500" : ''}`} key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
+          <button onClick={() => AddToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% original product</p>
+            <p>Cash on delivery available on this product</p>
+            <p>Easy return and exchange policy</p>
+
+          </div>
         </div>
       </div>
+
+      {/* ------- Description & Review Section-------  */}
+      <div className='mt-20'>
+        <div className='flex'>
+              <b className='border px-5 py-3 text-sm'>Description</b>
+              <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-600'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque deleniti quod sapiente veniam </p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, libero?</p>
+        </div>
+      </div>
+
+
+      {/* --------- display related product -------- */}
+
+      <RelatedProduct category={productData.category} subCategory={productData.subCategory}/>
+
     </div>
   ) : <div className='opacity-0'></div>
 }
