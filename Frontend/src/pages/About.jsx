31dc87aa177fb9c1089bb037fc840acd73036f01
Priyansh Gupta <priyanshgupta193@gmail.com>
2025-01-21 23:50:43 +0530

    AboutUSAndContactPage

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import Title from '../components/Title.jsx'
 
 function About() {
   return (
     <div>
-      
+        <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'}/>
+        </div>
+
+        <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dignissimos necessitatibus optio possimus ratione quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, omnis?</p>
+            <b className='text-gray-800'>Our Mission</b>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi unde alias cupiditate possimus, nostrum officiis voluptatum cum cumque reiciendis obcaecati.</p>
+          </div>
+        </div>
+
+        <div className='text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'}/>
+        </div>
+
+        <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurances :</b>
+            <p className='text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, eum harum numquam sed placeat impedit totam enim dolore iure praesentium!</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className='text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium corporis enim esse! Adipisci, nostrum maiores.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className='text-gray-600'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium corporis enim esse! Adipisci, nostrum maiores.</p>
+          </div>
+        </div>
     </div>
   )
 }
--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
 
 function Contact() {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 border-t'>
+        <Title text1={'Contact'} text2={'Us'}/>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'> Our Store</p>
+          <p className='text-gray-500'>54709 willms sterret</p>
+          <p className='text-gray-500'>Tel : (459-730-9887)</p>
+          <p className='font-semibold text-xl'>Carrers at Forever</p>
+          <p className='text-gray-500'>Learn more about jobs and openings</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hoverz;text-white'>Explore Jobs</button>
+        </div>
+      </div>
     </div>
   )
 }
